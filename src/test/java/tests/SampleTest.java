90a4f0a339dabe48429d16d442c8d0dadc278016
Federico Sánchez <fedesanchezvidarte@gmail.com>
2024-12-25 20:40:13 +0100

    Adding a SampleTest to test basics

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class SampleTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest extends BaseTest {
+
+    @Test
+    public void verifyHomePageTitle() {
+        page.navigate("https://the-internet.herokuapp.com/");
+        String title = page.title();
+        Assert.assertEquals(title,
+                "The Internet",
+                "Page title does not match!"
+        );
+    }
 }
